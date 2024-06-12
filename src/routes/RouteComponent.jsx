b777e3f33af242e7308bca2d06eda6a39976089d
talitharyn <talitharyn@gmail.com>
2024-06-12 17:56:57 +0700

    update fix error 401, create update delete product category

--- src/routes/RouteComponent.jsx
+++ src/routes/RouteComponent.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import axios from "axios";
 import setupAxios from "../utils/SetupAxios";
 import { BrowserRouter, useRoutes } from "react-router-dom";
@@ -6,15 +6,19 @@ import routeList from "./RouteList";
 import { useSelector, useDispatch } from "react-redux";
 
 const RouteRenderer = () => {
-  const dispatch = useDispatch();
   const token = useSelector((state) => state.auth.user.token);
-  const isLoggedIn = token !== null ? true : false;
+  const isLoggedIn = token !== null;
   const element = useRoutes(routeList(isLoggedIn));
-  setupAxios(axios, dispatch);
   return <React.Fragment>{element}</React.Fragment>;
 };
 
 export default function RouteComponent() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    setupAxios(axios, dispatch);
+  }, [dispatch]);
+
   return (
     <BrowserRouter>
       <RouteRenderer />
